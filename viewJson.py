4e0be9e9f605dc16549c01edcc4cbc339f6d4a03
Sonia Menéndez <sonia.menendez@alumnos.upm.es>
2024-12-17 23:05:46 +0100

    Subiendo archivos de plan diario

---
+++ viewJson.py
@@ -0,0 +1,18 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Carga del JSON desde el archivo local
+with open('C:/SONIA/4 ANO UNIVERSIDAD/Proyectos/datosPaciente.json', 'r', encoding='utf-8') as archivo:
+    datos = json.load(archivo)
+
+lista = []
+
+for sesion in datos.get("sesiones", []):
+    for ejercicio in sesion.get("ejercicios", []):
+        id_ejercicio = ejercicio.get("idEjercicio")
+        if id_ejercicio == 'ejercicio007':
+            lista.append(ejercicio["velocidadMaxima"])
+
+plt.plot(range(len(lista)), lista, marker='o', linestyle='-', color='b', label='Altura máxima')
+plt.show()
